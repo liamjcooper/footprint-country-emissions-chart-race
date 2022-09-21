b3bf930448180ca43ce50e8c92d6a1fb895e426a
Liam Cooper <minicooper22@hotmail.co.uk>
2022-09-21 21:08:06 +0100

    Re-enable root endpoint test

--- backend/tests/index.spec.js
+++ backend/tests/index.spec.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import assert from "assert";
 
 describe("Testing server", () => {
-  // it("Accessing root url should return 200", async () => {
-  //   const res = await axios.get("http://127.0.0.1:8000");
-  //   assert.equal(200, res.status);
-  // });
+  it("Accessing root url should return 200", async () => {
+    const res = await axios.get("http://127.0.0.1:8000");
+    assert.equal(200, res.status);
+  });
 
   it("Accessing emissions endpoint should return 200", async () => {
     const res = await axios.get(
